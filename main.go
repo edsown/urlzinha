7ae336de1dff4ad3028ebc1354f025bf954f64ea
edson.junior@serpro.gov.br <edson.junior@serpro.gov.br>
2026-07-30 17:06:56 -0300

    refactor: better logging for main

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"database/sql"
 	"github.com/edsown/urlzinha/business/repository"
 	"github.com/edsown/urlzinha/handler"
 	"github.com/edsown/urlzinha/service"
+	"log"
 	_ "modernc.org/sqlite"
 )
 
 func main() {
 
 	db, err := sql.Open("sqlite", "./app.db")
+	// TODO: fly.io
 	if err != nil {
-		fmt.Errorf("error with database creation %s", err)
+		log.Fatal("error creating the database %w", err)
 	}
 	defer db.Close()
 	err = db.Ping()
 	if err != nil {
-		fmt.Errorf("error pinging the database %w", err)
+		log.Fatal("error pinging the server", err)
 	}
 
 	query := `CREATE TABLE IF NOT EXISTS urls ( id INTEGER PRIMARY KEY AUTOINCREMENT, original_url TEXT NOT NULL, short_url TEXT UNIQUE, created_at DATETIME DEFAULT CURRENT_TIMESTAMP )`
 	_, err = db.Exec(query)
 	if err != nil {
-		fmt.Errorf("error creating table %w", err)
+		log.Fatal("error creating table %w", err)
 	}
 
-	fmt.Println("config loaded")
 	repo := repository.NewRepository(db)
 	svc := service.NewService(repo)
 	mux := http.NewServeMux()
@@ -37,6 +37,9 @@ func main() {
 	mux.HandleFunc("GET /shorten", func(w http.ResponseWriter, r *http.Request) {
 		creationHandler.HandleCreate(w, r)
 	})
-	http.ListenAndServe(":8080", mux)
+	log.Print("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("error starting server: %v", err)
+	}
 
 }
